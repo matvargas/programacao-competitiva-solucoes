62adca3d1b29e9c456b4ca647296eacdb0511755
Matheus Vargas <m6.vargas@gmail.com>
2019-03-06 16:48:13 -0300

    Commit de Exercício P2

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -3,39 +3,6 @@
 using namespace std;
 using std::endl;
 
-int isSlimp(string str) {
-
-    cout << "Entrou no isSlimp: " << str << endl;
-
-    bool result = false;
-
-    if(str[0] != 'A') {
-        cout << "primeira letra não é A: " << str[0] << endl;
-        result =  false;
-        return 0;  
-    }
-
-    if(str.length() == 2 && str[1] != 'H') {
-        cout << "Tamanho igual a 2 e segunda letra não é H: " << str[1] << endl;
-        result =  false;
-        return 0;
-    } else if (str.length() == 2 && str[1] == 'H') {
-        result =  true;
-        return 1;
-    }
-
-    if(str[1] == 'B') {
-        int subSlimpEndIndex = (str.find('C') - 2);
-        if(subSlimpEndIndex == -1) { result =  false; return 0; } 
-        if(!isSlimp(str.substr(2,subSlimpEndIndex))) {result =  false; return 0; }
-        result = true;
-        return 1;
-    }
-
-    return result;
-
-}
-
 int isSlump(string str) {
 
     cout << "testando slump " << str << endl;
@@ -66,9 +33,66 @@ int isSlump(string str) {
     return result;
 }
 
+int isSlimp(string str) {
+
+    cout << "Entrou no isSlimp: " << str << endl;
+
+    bool result = false;
+
+    if(str[0] != 'A') {
+        cout << "primeira letra não é A: " << str[0] << endl;
+        result =  false;
+        return 0;  
+    }
+
+    if (str[1] == 'H') {
+        cout << "Slimp identificado " << str.substr(0, 2) << endl;
+        cout << "Testando se o restante e slumpy " << str.substr(2) << endl;
+        if(!isSlump(str.substr(2))) {result =  false; return 0; }
+        result = true;
+        return 1;
+    }
+
+    if(str[1] == 'B') {
+        int subSlimpEndIndex = (str.find('C') - 2);
+        if(subSlimpEndIndex == -1) { result =  false; return 0; } 
+        if(!isSlimp(str.substr(2,subSlimpEndIndex))) {result =  false; return 0; }
+        result = true;
+        return 1;
+    } else {
+        int subSlimpEndIndex = (str.find('C') - 2);
+        if(subSlimpEndIndex == -1) { result =  false; return 0; } 
+        if(!isSlump(str.substr(2,subSlimpEndIndex))) {result =  false; return 0; }
+        result = true;
+        return 1;
+    }
+
+    return result;
+
+}
+
 int main() {
-    string str = "";
-    getline(cin, str);
-    cout << isSlump(str) << endl;
+    
+    int slurpyCount = 0;
+    int i = 0;
+    cin >> slurpyCount;
+    string textsToVerify[slurpyCount];
+    for(i = 0; i < slurpyCount; i++) {
+        string str = ""; 
+        cin >> str;
+        textsToVerify[i] = str;
+    } 
+    
+    cout << "SLURPYS OUTPUT" << endl;
+    for(i = 0; i < slurpyCount; i++) {
+        if(isSlimp(textsToVerify[i])) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+        cout << "______" << endl;
+        cout << endl;
+    }
+    cout << "END OF OUTPUT" << endl;
     return 0;
 }
