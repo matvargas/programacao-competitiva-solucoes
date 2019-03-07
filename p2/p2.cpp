5c49fea07434b5944f5334d47165e10fd6a58425
Matheus Vargas <m6.vargas@gmail.com>
2019-03-07 11:35:04 -0300

    Commit de Exercício P2

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -6,100 +6,51 @@ using std::endl;
 
 bool shouldShowDebug;
 bool result = false;
-string actualyTextOnVerification = "";
-int slimpCount = 0;
+bool slimpVerification, slumpEvaluation;
 
 bool isSlump(string str) {
 
     if(shouldShowDebug) cout << "testando slump " << str << endl;
-
-    if(str[0] != 'D' && str[0] != 'E') { return false; }
-    if(str[1] != 'F' || str[str.size() - 1] == 'F') { return false; }
-    int lastIndexOfF = -1;
-    int numberOfFs = 0;
-    for(std::string::size_type i = str.find('F'); i < str.size(); ++i) {
-        if(str[i] != 'F') {
-            lastIndexOfF = i;
-            if(shouldShowDebug) cout << "Ultima posicao do F " << lastIndexOfF << endl;
-            break;
-        } else {
-            numberOfFs ++;
-        }
-    }
-    if(str[lastIndexOfF] != 'G') {
-        if(shouldShowDebug) cout << "Char na posicao " << lastIndexOfF << " é " << str[lastIndexOfF] << endl;
-        if(!isSlump(str.substr(lastIndexOfF))) { return false; }
-    } else if (str[str.length() - 1] != 'G') {
-        if(shouldShowDebug) cout << "Char na posicao " << str.length() - 1 << " é " << str[str.length() - 1] << " por isso não é slumpy" << endl;
-        return false;
-    } else {
-        if(shouldShowDebug) cout << "Char na posicao " << lastIndexOfF << " é " << str[lastIndexOfF] << endl;
-        result = true;
-    }
-
-
-    if(shouldShowDebug) if(result) {cout << str << " e slumpy" << endl; }
     return result;
 }
 
 bool isSlimp(string str) {
 
-    if(shouldShowDebug) cout << "Avaliando isSlimp: " << str << endl;
-    if(shouldShowDebug) cout << slimpCount << endl;
+    if(shouldShowDebug) cout << "Slimp - Evaluating : " << str << " - size:" << str.length() <<endl;
 
     if(str[0] != 'A') {
-        if(shouldShowDebug) cout << "primeira letra não é A: " << str[0] << endl;
-        return 0;  
+        if(shouldShowDebug) cout << "The evaluated text : " << str << " is not a slimp, does not start with an 'A' " << endl;
+        return false;
     }
 
-    if (str[1] == 'H') {
-        if(str.length() > 2) {
-            if(shouldShowDebug) cout << "Testando se o restante e slumpy " << str.substr(2) << endl;
-            if(!isSlump(str.substr(2))) { return false; }
-            return true;
+    if(str.length() == 2) {
+        if(str[1] != 'H') {
+            if(shouldShowDebug) cout << "The evaluated text : " << str << " is not a slimp, it has length 2 and second char is not 'H' " << endl;
+            return false;
         } else {
-            if(shouldShowDebug) cout << "Slimp identificado " << str.substr(0, 2) << endl;
-            if(str.compare(actualyTextOnVerification) == 0) {
+            if(shouldShowDebug) cout << "The evaluated text : " << str << " is a slimp case 'AH' " << endl;
+            slimpVerification = true;
+            result = true;
+        }
+    } else {
+        if (str[1] == 'B') {
+            if(shouldShowDebug) cout << "Slimp is 'BC' type : " << str << endl;
+            int lastCIndex = str.find_last_of('C');
+            if(shouldShowDebug) cout << "Finding the potential end of the slimp : " << lastCIndex << " is the last occurrence of 'C' " << endl;
+            string subSlimp = str.substr(2, lastCIndex - 2);
+            if(shouldShowDebug) cout << "Evaluating potential subSlimp : " << subSlimp <<  endl;
+            isSlimp(subSlimp);
+        } else {
+            if(str[1] != 'D' && str[1] != 'E') {
+                if(shouldShowDebug) cout << "The evaluated text : " << str << " is not a slimp, after an 'A' has no 'B', 'D' or 'E' " << endl;
                 return false;
             } else {
-                return true;
+                
             }
-        }
+        } 
     }
 
-    if(str[1] == 'B') {
-        slimpCount++;
-        int subSlimpEndIndex = (str.find('C') - slimpCount);
-        if(subSlimpEndIndex == -1) { return false; } 
-        if(!isSlimp(str.substr(2,subSlimpEndIndex - 1))) {return false; }
-        if(subSlimpEndIndex + 1 == str.length() - 1){ return false; } // Is just a slimpy
-
-        int cCounter = 0;
-        if(subSlimpEndIndex != (str.length() - 1)) {
-            int sufixLength = str.substr(subSlimpEndIndex + 1 ).length();
-            if(shouldShowDebug) cout << "Sobrou " << str.substr(subSlimpEndIndex + 1 ) << " de tamanho " << sufixLength <<  endl;
-            for (int i = 0; i < sufixLength - 1; i++) {
-                if(str.substr(subSlimpEndIndex + 1)[i] == 'C') {
-                    cCounter ++;
-                }
-            }
-            if(shouldShowDebug) cout << "Existem  " << cCounter << " Cs no sufixo restante"  <<  endl;
-        }
-
-        if(!isSlump(str.substr(subSlimpEndIndex + 1 + cCounter))) { return false; }
-
-        result = true;
-    } else {
-        int subSlimpEndIndex = (str.find('C'));
-        if(subSlimpEndIndex == -1) { return false; } 
-        if(!isSlump(str.substr(1,subSlimpEndIndex - 1))) { return false; }
-
-        if(subSlimpEndIndex != (str.length() - 1)) {
-            if(!isSlump(str.substr(subSlimpEndIndex + 1))) {return false; }
-        }
-        result = true;
-    }
-
+    if(shouldShowDebug) cout << "Slimp - result : " << result << endl;
     return result;
 
 }
@@ -119,9 +70,14 @@ int main() {
     
     cout << "SLURPYS OUTPUT" << endl;
     for(i = 0; i < slurpyCount; i++) {
-        actualyTextOnVerification = textsToVerify[i];
-        slimpCount = 0;
-        if(isSlimp(textsToVerify[i])) {
+        bool slimpAswer = isSlimp(textsToVerify[i]);
+        bool answer = slimpAswer && slimpVerification && slumpEvaluation;
+        if(shouldShowDebug) cout << "After process the results are: " << endl;
+        if(shouldShowDebug) cout << "isSlimp: "<< slimpAswer << endl;
+        if(shouldShowDebug) cout << "slimpVerification: "<< slimpVerification << endl;
+        if(shouldShowDebug) cout << "slumpVerification: "<< slumpEvaluation << endl;
+        if(shouldShowDebug) cout << "Answer is : "<< answer << endl;
+        if(answer) {
             cout << "YES" << endl;
         } else {
             cout << "NO" << endl;
